e50ff2511dbfc060d467ef4ec982a34b6b86857a
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-10 01:43:46 +0700

    PolyFill and Alpha blending

--- ArcadeApp/Graphics/Screen.cpp
+++ ArcadeApp/Graphics/Screen.cpp
@@ -9,11 +9,14 @@
 #include "Screen.h"
 #include "Vec2D.h"
 #include "Line2D.h"
+#include "Triangle.h"
+#include "AARectangle.h"
+#include "Circle.h"
 #include "Utils.h"
 #include <SDL.h>
 #include <cassert>
 #include <cmath>
-
+#include <algorithm>
 
 Screen::~Screen()
 {
@@ -157,11 +160,178 @@ void Screen::Draw(const Line2D& line, const Color& color)
 	}
 }
 
+void Screen::Draw(const Triangle& triangle, const Color& color, bool fill, const Color& fillColor)
+{
+	if (fill)
+	{
+		FillPoly(triangle.GetPoints(), fillColor);
+	}
+
+	Line2D p0p1 = Line2D(triangle.GetP0(), triangle.GetP1());
+	Line2D p1p2 = Line2D(triangle.GetP1(), triangle.GetP2());
+	Line2D p2p0 = Line2D(triangle.GetP2(), triangle.GetP0());
+
+	Draw(p0p1, color);
+	Draw(p1p2, color);
+	Draw(p2p0, color);
+}
+
+void Screen::Draw(const AARectangle& rect, const Color& color, bool fill, const Color& fillColor)
+{
+	if (fill)
+	{
+		FillPoly(rect.GetPoints(), fillColor);
+	}
+	std::vector<Vec2D> points = rect.GetPoints();
+
+	Line2D p0p1 = Line2D(points[0], points[1]);
+	Line2D p1p2 = Line2D(points[1], points[2]);
+	Line2D p2p3 = Line2D(points[2], points[3]);
+	Line2D p3p0 = Line2D(points[3], points[0]);
+
+	Draw(p0p1, color);
+	Draw(p1p2, color);
+	Draw(p2p3, color);
+	Draw(p3p0, color);
+}
+
+void Screen::Draw(const Circle& circle, const Color& color, bool fill, const Color& fillColor)
+{
+	static unsigned int NUM_CIRCLE_SEGMENTS = 30;
+
+	std::vector<Vec2D> circlePoints;
+	std::vector<Line2D> lines;
+
+	float angle = TWO_PI / float(NUM_CIRCLE_SEGMENTS);
+
+	Vec2D p0 = Vec2D(circle.GetCenterPoint().GetX() + circle.GetRadius(), circle.GetCenterPoint().GetY());
+	Vec2D p1 = p0;
+	Line2D nextLineToDraw;
+	//circlePoints.push_back(p0);
+
+	for (unsigned int i = 0; i < NUM_CIRCLE_SEGMENTS; ++i)
+	{
+		p1.Rotate(angle, circle.GetCenterPoint());
+		nextLineToDraw.SetP1(p1);
+		nextLineToDraw.SetP0(p0);
+
+		lines.push_back(nextLineToDraw);
+		p0 = p1;
+		circlePoints.push_back(p0);
+	}
+
+	if (fill)
+	{
+		FillPoly(circlePoints, fillColor);
+	}
+
+	for (const Line2D& line : lines)
+	{
+		Draw(line, color);
+	}
+}
+
 void Screen::ClearScreen()
 {
 	assert(moptrWindow);
 	if(moptrWindow)
 	{
 		SDL_FillRect(mnoptrWindowSurface, nullptr, mClearColor.GetPixelColor());
+	}
+}
+
+void Screen::FillPoly(const std::vector<Vec2D>& points, const Color& color)
+{
+	if (points.size() > 0)
+	{
+		float top = points[0].GetY();
+		float bottom = points[0].GetY();
+		float right = points[0].GetX();
+		float left = points[0].GetX();
+		// find boundary of a shape
+		for (size_t i = 1; i < points.size(); ++i)
+		{
+			if (points[i].GetY() < top)
+			{
+				top = points[i].GetY();
+			}
+
+			if (points[i].GetY() > bottom)
+			{
+				bottom = points[i].GetY();
+			}
+
+			if (points[i].GetX() < left)
+			{
+				left = points[i].GetX();
+			}
+
+			if (points[i].GetX() > right)
+			{
+				right = points[i].GetX();
+			}
+		}
+
+		// loop the top of a shape to the botton
+		for (int pixelY = top; pixelY < bottom; ++pixelY)
+		{
+			std::vector<float> nodeXVec;
+
+			size_t j = points.size() - 1;
+
+			for (size_t i = 0; i < points.size(); ++i)
+			{
+				float pointiY = points[i].GetY();
+				float pointjY = points[j].GetY();
+
+
+				if ((pointiY <= (float)pixelY && pointjY > (float)pixelY) || (pointjY <= (float)pixelY && pointiY > (float)pixelY))
+				{
+					float denom = pointjY - pointiY;
+					if (IsEqual(denom, 0))
+					{
+						continue;
+					}
+
+					float x = points[i].GetX() + (pixelY - pointiY) / (denom) * (points[j].GetX() - points[i].GetX());
+					nodeXVec.push_back(x);
+				}
+
+				j = i;
+			}
+
+			std::sort(nodeXVec.begin(), nodeXVec.end(), std::less<>());
+
+			for (size_t k = 0; k < nodeXVec.size(); k += 2)
+			{
+				if (nodeXVec[k] > right)
+				{
+					break;
+				}
+
+				if (nodeXVec[k + 1] > left)
+				{
+					if (nodeXVec[k] < left)
+					{
+						nodeXVec[k] = left;
+					}
+					if (nodeXVec[k + 1] > right)
+					{
+						nodeXVec[k + 1] = right;
+					}
+
+					//Line2D line = {Vec2D(nodeXVec[k], pixelY), Vec2D(nodeXVec[k+1], pixelY)};
+					//Draw(line, color);
+					for (int pixelX = nodeXVec[k]; pixelX < nodeXVec[k + 1]; ++pixelX)
+					{
+						Draw(pixelX, pixelY, color);
+					}
+				}
+			}
+		}
+
+
+
+
 	}
 }
--- ArcadeApp/Graphics/Screen.h
+++ ArcadeApp/Graphics/Screen.h
@@ -13,8 +13,12 @@
 #include "Color.h"
 #include <vector>
 
+
 class Vec2D;
 class Line2D;
+class Triangle;
+class AARectangle;
+class Circle;
 struct SDL_Window;
 struct SDL_Surface;
 
@@ -35,14 +39,17 @@ public:
 	void Draw(int x, int y, const Color& color);
 	void Draw(const Vec2D& point, const Color& color);
 	void Draw(const Line2D& line, const Color& color);
+	void Draw(const Triangle& triangle, const Color& color, bool fill = false, const Color& fillColor = Color::White());
+	void Draw(const AARectangle& rect, const Color& color, bool fill = false, const Color& fillColor = Color::White());
+	void Draw(const Circle& circle, const Color& color, bool fill = false, const Color& fillColor = Color::White());
 
 private:
 
-	Screen(const Screen& screen);
-	Screen& operator=(const Screen& screen);
+	//Screen(const Screen& screen);
+	//Screen& operator=(const Screen& screen);
 
 	void ClearScreen();
-	//void FillPoly(const std::vector<Vec2D>& points, const Color& color);
+	void FillPoly(const std::vector<Vec2D>& points, const Color& color);
 	uint32_t mWidth;
 	uint32_t mHeight;
 
