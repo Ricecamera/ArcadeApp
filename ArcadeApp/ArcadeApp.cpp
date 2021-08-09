577059c7360e55ef60f01f5d3d3d38686546b6a4
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-09 23:42:59 +0700

    Add Shapes class to the programs

--- ArcadeApp/ArcadeApp.cpp
+++ ArcadeApp/ArcadeApp.cpp
@@ -1,13 +1,57 @@
-// ArcadeApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//============================================================================
+// Name        : ArcadeApp.cpp
+// Author      : Serge Lanisquot
+// Version     :
+// Copyright   :
+// Description : Hello World in C++, Ansi-style
+//============================================================================
 
 #include <iostream>
 #include <SDL.h>
-#include <stdio.h>
+#undef main
 
-int main(int arg, char* args[])
+#include "Color.h"
+#include "Screen.h"
+#include "Line2D.h"
+
+const int SCREEN_WIDTH = 224;
+const int SCREEN_HEIGHT = 288;
+const int MAGNIFICATION = 2;
+
+using namespace std;
+
+int main(int argc, const char* argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    std::cout << "Hello World!\n";
-    return 0;
+	Screen theScreen;
+
+	theScreen.Init(SCREEN_WIDTH, SCREEN_HEIGHT, MAGNIFICATION);
+
+	Line2D line = { Vec2D(0, 0), Vec2D(SCREEN_WIDTH, SCREEN_HEIGHT) };
+
+	float rotateAngle = M_PI;
+	line.RotateBy(rotateAngle);
+
+	theScreen.Draw(line, Color::White());
+	theScreen.SwapScreens();
+
+	SDL_Event sdlEvent;
+	bool running = true;
+
+	
+	while (running)
+	{
+		while (SDL_PollEvent(&sdlEvent))
+		{
+			
+
+			switch (sdlEvent.type)
+			{
+			case SDL_QUIT:
+				running = false;
+				break;
+			}
+		}
+	}
+
+	return 0;
 }
